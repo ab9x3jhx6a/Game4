327b726d1f61c73dd7cc54d50044ba515226af26
heskey30 <heskey333@gmail.com>
2015-12-05 16:14:10 -0500

    Preliminary spike mutation testing.

---
+++ Game4/Assets/DealDamage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DealDamage : MonoBehaviour {
+
+	public float damage = 5;
+	public float knockback = 50;
+
+
+
+	void OnCollisionEnter(Collision other){
+		Stats temp = other.gameObject.GetComponent<Stats>();
+		print ("collision detected.");
+		if(temp){
+			
+			print ("collision is a valid target!");
+			temp.takeDamage(damage);
+			temp.rigidbody.AddExplosionForce(knockback,transform.position,10);
+		}
+
+	}
+	// Use this for initialization
+/*	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+}
