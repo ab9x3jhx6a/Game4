35bf883a7bb9b5e3f688b6c495091527b27dfae7
charder <chardergaming@gmail.com>
2015-12-05 16:02:18 -0500

    Updated Mutations a bit. Changed a few prefabs.

---
+++ Game4/Assets/Health.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : Mutation {
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Game4/Assets/Mutations.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mutations : MonoBehaviour {
+	public List<string> mutations; //list of mutations by name
+
+}
