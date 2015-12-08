ea7639183c9d090e467325b2222b35b166b1cd83
charder <chardergaming@gmail.com>
2015-12-07 23:03:03 -0500

    Mutation bug fixes and random add/remove of mutations.

--- Game4/Assets/Scripts/Mutations.cs
+++ Game4/Assets/Scripts/Mutations.cs
@@ -5,10 +5,39 @@ using System.Collections.Generic;
 public class Mutations : MonoBehaviour {
 	public List<string> mutations; //list of mutations by name
 	public List<Mutation> mutationComponents; //list of components of mutations
+	public bool radiation = false; //whether or not this bacteria is affected by radiation
 
-	public void Start() {
+	public void Awake() {
 		if (gameObject.name != "Globals") {
-			addMutation ();
+			foreach (Transform t in transform) {
+				Destroy(t.gameObject);
+			}
+			int mutRoll = Random.Range (1,11); //rolls an int between 1-10, simulates 10% chance for each number
+			bool mutateBool = false; //whether or not to add a mutation
+			bool removeMutateBool = false; //whether or not to take away a mutation
+			if (radiation) {
+				if (mutRoll > 2 && mutRoll <= 5) {
+					mutateBool = true;
+				}
+				else if (mutRoll <= 2) {
+					removeMutateBool = true;
+				}
+			}
+			else {
+				if (mutRoll == 2) {
+					mutateBool = true;
+				}
+				else if (mutRoll == 1) {
+					removeMutateBool = true;
+				}
+			}
+			if (mutateBool) {
+				addMutation();
+			}
+			else if (removeMutateBool) {
+				removeMutation();
+			}
+
 		} 
 		else {
 			Component[] mutationComps = this.gameObject.GetComponents (typeof(Mutation)); //gets the list of all mutations to name check
@@ -20,8 +49,10 @@ public class Mutations : MonoBehaviour {
 				}
 				((Mutation) mutationComps[i]).enabled = false; //disables the component as it's added
 			}
+			radiation = false;
 		}
 	}
+
 	//use this function for deciding what mutations you do or don't have
 	public List<string> differenceList (List<string> start, List<string> subtract) {
 		List<string> diffList = start;
@@ -51,12 +82,27 @@ public class Mutations : MonoBehaviour {
 		List<string> NewMutations = differenceList (globalref.mutations, mutations); //finds which mutations this bacteria doesn't have
 		Debug.Log ("Mutation Size: " + NewMutations.Count);
 		int num = Random.Range (0, NewMutations.Count); //rolls a random mutation within the range of ones you don't have
-		string newMutName = NewMutations [num];
-		Debug.Log (newMutName);
-		List<Mutation> mutationGlobals = globalref.mutationComponents; //gets the list of all mutations to name check
-		for (int i = 0; i < mutationGlobals.Count; i++) {
-			if (mutationGlobals[i].mutationName == newMutName) {
-				this.CopyComponent(mutationGlobals[i],this.gameObject);
+		if (NewMutations.Count > 0) {
+			string newMutName = NewMutations [num];
+			Debug.Log (newMutName);
+			List<Mutation> mutationGlobals = globalref.mutationComponents; //gets the list of all mutations to name check
+			for (int i = 0; i < mutationGlobals.Count; i++) {
+				if (mutationGlobals [i].mutationName == newMutName) {
+					this.CopyComponent (mutationGlobals [i], this.gameObject);
+				}
+			}
+		}
+	}
+
+	public void removeMutation() {
+		if (mutations.Count > 0) {
+			int randRoll = Random.Range (0,mutations.Count);
+			Mutation[] mutList = (Mutation[])(this.gameObject.GetComponents(typeof(Mutation))); 
+			foreach (Mutation mut in mutList) {
+				if (mut.mutationName == mutations[randRoll]) {
+					mutations.Remove(mutations[randRoll]); //remove the mutation's name from its list of mutations
+					Destroy (mut); //remove the component itself from this object so it never runs to begin with since this is called in Awake()
+				}
 			}
 		}
 	}
