52192a0d68238bd79e7b0cc4e978c36945ba2e6f
charder <chardergaming@gmail.com>
2015-12-09 04:07:32 -0500

    Music added.

---
+++ Game4/Assets/Music/MenuMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MenuMusic : MonoBehaviour {
+	AudioSource sound;
+	public AudioClip backMusic;
+	// Use this for initialization
+	void Start () {
+		sound.Play ();
+		sound.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
