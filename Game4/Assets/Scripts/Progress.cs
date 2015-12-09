ebe3869ca803a199806dfc59abb323c1a3f3143c
heskey30 <heskey333@gmail.com>
2015-12-09 12:49:18 -0500

    Changed the required predators to win - you now need 50.

--- Game4/Assets/Scripts/Progress.cs
+++ Game4/Assets/Scripts/Progress.cs
@@ -6,16 +6,18 @@ public class Progress : MonoBehaviour {
 
     Global global;
     Text progressGoal;
-
+	Global[] globals;
 	// Use this for initialization
 	void Start () {
         progressGoal = GetComponent<Text>();
         global = GameObject.FindObjectOfType<Global>();
-        global.endGoal = 20;
+        globals = GameObject.FindObjectsOfType<Global>();
+        //global.endGoal = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		print (global.endGoal);
 	    progressGoal.text = global.extractedSoFar + "/" + global.endGoal;
 	}
 }
